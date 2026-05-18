d5151c542da862e87d733d5fc666baed8295b13e
Henrique <spellctro@gmail.com>
2026-05-18 20:03:45 -0300

    refactor: deleting sample files

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#444' }}>
+            <Text style={{ color: '#f2f2f2', fontSize: 48 }}>Olá Mundo!</Text>
+        </View>
+    )
+}
