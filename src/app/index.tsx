635414be249dc2d25e7d13843aeb853292a9c63f
Henrique <spellctro@gmail.com>
2026-05-18 22:20:57 -0300

    feat: criando storage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,41 @@
 import ScrollArea from "@/components/ScrollArea"
+import placaService, { Placa, PlacaCreate } from "@/services/placas"
+import { useEffect, useState } from "react"
 import { Text, TextInput, TouchableOpacity, View } from "react-native"
 import { styles } from "./styles"
 
 export default function Index() {
+
+    const [placas, setPlacas] = useState<Placa[]>([])
+    const [input, setInput] = useState('')
+
+    async function load() {
+        const response = await placaService.find()
+        setPlacas(response)
+    }
+
+    async function create() {
+        if (!input.trim()) return
+
+        const newPlaca: PlacaCreate = {
+            placa: input,
+            checkIn: new Date(),
+            checkOut: null
+        }
+
+        const response = await placaService.create(newPlaca)
+        setPlacas(response)
+        setInput('')
+    }
+
+    useEffect(() => { load() }, [])
+
     return (
         <ScrollArea style={styles.container}>
             <Text style={styles.logoText}>Vagas do BoxTo</Text>
             <View style={styles.inputView}>
-                <TextInput style={styles.inputText} />
-                <TouchableOpacity style={styles.inputButton}>
+                <TextInput onChangeText={text => setInput(text.toUpperCase())} value={input} style={styles.inputText} />
+                <TouchableOpacity onPress={create} style={styles.inputButton}>
                     <Text style={styles.inputButtonText}>
                         Cadastrar
                     </Text>
@@ -16,14 +43,30 @@ export default function Index() {
             </View>
 
             {/* list */}
-            <View style={styles.listContainer}>
-                <Text style={styles.listPlaca}>ABC-1234</Text>
-                <Text style={styles.listCheckIn}>11:30</Text>
-                {/* <Text>12:30</Text> */}
-                <TouchableOpacity style={styles.listButton}>
-                    <Text style={styles.listButtonText}>Finalizar</Text>
-                </TouchableOpacity>
-            </View>
+            {placas.map(p => {
+                const dataCheckIn = new Date(p.checkIn)
+                const dataCheckOut = p.checkOut ? new Date(p.checkOut) : ''
+                const horas = String(dataCheckIn.getHours()).padStart(2, '0')
+                const minutos = String(dataCheckIn.getMinutes()).padStart(2, '0')
+
+                return (
+                    <View key={p.id} style={styles.listContainer}>
+
+                        <Text style={styles.listCheckIn}>
+                            {`${horas}:${minutos}`}
+                        </Text>
+                        <Text style={styles.listCheckIn}>
+                            {`${horas}:${minutos}`}
+                        </Text>
+
+                        <Text style={styles.listPlaca}>{p.placa}</Text>
+
+                        <TouchableOpacity style={styles.listButton}>
+                            <Text style={styles.listButtonText}>Finalizar</Text>
+                        </TouchableOpacity>
+                    </View>
+                )
+            })}
         </ScrollArea >
     )
 }
--- src/services/placas.ts
+++ src/services/placas.ts
@@ -2,8 +2,20 @@ import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const PLACAS_STORAGE_KAY = "@estacionamento:placas"
 
+export interface Placa {
+    id: string
+    placa: string
+    checkIn: Date
+    checkOut: Date | null
+}
+export interface PlacaCreate {
+    placa: string
+    checkIn: Date
+    checkOut: Date | null
+}
+
 // find
-async function find() {
+async function find(): Promise<Placa[]> {
     try {
         const storage = await AsyncStorage.getItem(PLACAS_STORAGE_KAY)
         return storage ? JSON.parse(storage) : []
@@ -12,5 +24,44 @@ async function find() {
     }
 }
 // create
+async function create(placa: PlacaCreate): Promise<Placa[]> {
+    try {
+        const id = Math.random().toString().replace('.', '')
+        const novaPlaca = { id, ...placa }
+        const storage = await find()
+        const newStorage = [...storage, novaPlaca]
+        await AsyncStorage.setItem(PLACAS_STORAGE_KAY, JSON.stringify(newStorage))
+        return newStorage
+    } catch (error) {
+
+    }
+    return []
+}
+
+// close
+async function close(placa: Placa): Promise<Placa[]> {
+    try {
+        const novaPlaca: Placa = {
+            ...placa,
+            checkOut: new Date()
+        }
+        const storage = await find()
+        const newStorage = [...storage, novaPlaca]
+        await AsyncStorage.setItem(PLACAS_STORAGE_KAY, JSON.stringify(newStorage))
+        return newStorage
+    } catch (error) {
+
+    }
+    return []
+}
 // update
 // delete
+
+const placaService = {
+    find,
+    create,
+    close
+
+}
+
+export default placaService
